b4391892c46b8164303b91eb2d3e6d158e405a0f
Andreas Olsson <olsson.andreas@gmail.com>
2024-01-26 11:31:44 +0100

    update test

--- internal/postprocessor/postprocessor_test.go
+++ internal/postprocessor/postprocessor_test.go
@@ -4,7 +4,7 @@ import "testing"
 
 func TestNewFilenameNoSlash(t *testing.T) {
     expected := "/foo/bar/20220718_12345671_SomeStuff.mp4"
-    fileName := "20220718_1234567_SomeStuff.mp4"
+    fileName := "20220718_1234567_SomeStuff"
     filePath := "/foo/bar"
     res := getNewFilePath(filePath, fileName)
 
@@ -14,7 +14,7 @@ func TestNewFilenameNoSlash(t *testing.T) {
 }
 func TestNewFilename(t *testing.T) {
     expected := "/foo/bar/20220718_12345671_SomeStuff.mp4"
-    fileName := "20220718_1234567_SomeStuff.mp4"
+    fileName := "20220718_1234567_SomeStuff"
     filePath := "/foo/bar/"
     res := getNewFilePath(filePath, fileName)
 
